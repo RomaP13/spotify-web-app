083c485bbd335cbf394bcaee4b1f79546915f26d
Roman Pomazan <romapomazan1310@gmail.com>
2025-01-14 07:29:23 +0200

    feat: add simple usage of Spotify API

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
-from app.api.routes import callback, login, root
+from app.api.routes import callback, login, playlists, root, users
 
 api_router = APIRouter()
-api_router.include_router(login.router)
-api_router.include_router(callback.router)
-api_router.include_router(root.router)
+routes = [
+    callback,
+    login,
+    playlists,
+    root,
+    users,
+]
+for r in routes:
+    api_router.include_router(r.router)
