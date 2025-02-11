a3e9069d4878af4fc2c1b7d756b31ac7f1bcdb98
Roman Pomazan <romapomazan1310@gmail.com>
2025-02-11 11:52:14 +0200

    chore(logging): add configuration for logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from loguru import logger
 
 from app.api.main import api_router
 from app.api.middlewares import LoggingMiddleware
 from app.core.database import create_db_and_tables
 
+# Configure logging
+logger.add(
+    "logs/debug.log", rotation="10 MB", retention="40 days", level="INFO"
+)
 
 app = FastAPI()
 
@@ -18,10 +23,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 app.include_router(api_router)
 
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+    logger.info("🚀 FastAPI server is starting...")
