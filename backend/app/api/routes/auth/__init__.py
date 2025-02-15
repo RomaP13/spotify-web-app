838b5de8743298e2c634c93bec6ba01a5d72440d
Roman Pomazan <romapomazan1310@gmail.com>
2025-02-15 19:27:37 +0200

    chore(routes): add __all__ to __init__.py files to specify exported routers

--- backend/app/api/routes/auth/__init__.py
+++ backend/app/api/routes/auth/__init__.py
@@ -1,2 +1,4 @@
 from .callback import router as callback_router
 from .login import router as login_router
+
+__all__ = ["callback_router", "login_router"]
--- backend/app/api/routes/users/__init__.py
+++ backend/app/api/routes/users/__init__.py
@@ -1,1 +1,3 @@
 from .users import router as users_router
+
+__all__ = ["users_router"]
